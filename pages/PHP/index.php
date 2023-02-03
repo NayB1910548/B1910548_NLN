f0965f8c7eb8fb46107421cc5e4f97c781754bb3
LaNgocNayB1910548 <nayb1910548@student.ctu.edu.vn>
2023-02-03 17:07:08 +0700

    create project

---
+++ pages/PHP/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <title>RUOK-Store</title>
+     <link rel="stylesheet" href="">
+     <link rel="shortcut icon" href="pages/Foods/logo-ruok.svg" type="image/vnd.microsoft.icon">
+
+
+</head>
+
+<body>
+     
+
+
+</body>
+
+</html>
